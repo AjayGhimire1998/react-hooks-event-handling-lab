76a7dee38e4eab7f5039743c463607c7a52c3526
AjayGhimire <ajayghimire1998@gmail.com>
2022-03-28 20:34:28 +1100

    done with the lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import react from "react";
+
+export default function EyesOnMe(){
+    return(
+    <button type="button" 
+    name="button"
+    onFocus={ () => {
+        console.log("Good!")
+    }}
+    onBlur={ () => {
+        console.log("Hey! Eyes on me!")
+    }}>
+        Eyes on me
+    </button>
+    )
+};
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
-// Code Keypad Component Here
+import react from "react";
+
+export default function Keypad() {
+
+    return (
+        <div>
+            <input type="password" 
+            name="password" 
+            placeholder="Enter Password"
+            onChange={ () =>{
+                console.log("Entering password...")
+            }}>
+            </input>
+        </div>
+    )
+}
